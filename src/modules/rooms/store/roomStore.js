8971cff10416336a4a6adf76052c4896aec1667d
hello <sombathmeoun96@gmail.com>
2026-06-05 15:12:24 +0700

    Reapply "Merge remote-tracking branch 'origin/feature/public-customer-ui' into dev"
    This reverts commit 1d72a82df7fe1fba478500e5f9b4f7f35e9c5eff.

--- src/modules/rooms/store/roomStore.js
+++ src/modules/rooms/store/roomStore.js
@@ -52,6 +52,25 @@ export const useRoomStore = defineStore("rooms", () => {
         selectedPropertyId.value = propertyId;
     };
 
+    const addRoom = (room) => {
+        rooms.value.unshift(room);
+    };
+
+    const updateRoom = (roomId, updatedRoom) => {
+        const index = rooms.value.findIndex((room) => room.id === roomId);
+
+        if (index !== -1) {
+            rooms.value[index] = {
+                ...rooms.value[index],
+                ...updatedRoom,
+            };
+        }
+    };
+
+    const deleteRoom = (roomId) => {
+        rooms.value = rooms.value.filter((room) => room.id !== roomId);
+    };
+
     const fetchRoomsData = async () => {
         loading.value = true;
         error.value = null;
@@ -87,5 +106,8 @@ export const useRoomStore = defineStore("rooms", () => {
         fetchRoomsData,
         getRoomMeta,
         setSelectedPropertyId,
+        addRoom,
+        updateRoom,
+        deleteRoom,
     };
 });
