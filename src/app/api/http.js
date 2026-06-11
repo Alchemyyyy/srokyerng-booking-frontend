7eb5a28da6f2003606b785a3158003e68b236a25
leakhena-bou <leakhenabou2005@gmail.com>
2026-06-11 15:03:26 +0700

    chore: stop tracking production env

--- src/app/api/http.js
+++ src/app/api/http.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    (import.meta.env.PROD ? "https://api-srokyerng.devspace.linkpc.net/api" : "http://localhost:5001/api"),
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
